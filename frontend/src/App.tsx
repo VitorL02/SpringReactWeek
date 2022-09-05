2bdeb3f9d0b8b73124bddeaa1ff78928e4ff25a9
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-09-05 15:51:20 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import NotificationButton from "./components/notification-button/notification-button"
 
 
 function App() {
   return (
-    <h1>Hello Word</h1>
-  )
+    <>
+      <h1>Hello Word</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App
