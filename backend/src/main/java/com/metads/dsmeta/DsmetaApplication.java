819a2370fe3e0decdeb24bda1bc2927aad565e9e
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-09-05 14:30:10 -0300

    Project created

---
+++ backend/src/main/java/com/metads/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.metads.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/metads/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metads.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
