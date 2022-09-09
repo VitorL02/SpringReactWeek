a7f25c22f1e7eeb4d754c6d83aee380d2a1523fd
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-09-09 10:49:13 -0300

    Send notification

--- frontend/src/components/notification-button/notification-button.tsx
+++ frontend/src/components/notification-button/notification-button.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
 import './notification-button-style.css';
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(saleId: number) {
+    axios.get(`${BASE_URL}/sales/${saleId}/notification`).then(response => {
+        console.log(response.data)
+    })
+}
+
+function NotificationButton({ saleId }: Props) {
+
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
